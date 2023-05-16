0a26d50396750ecc24a79f0d0a01b9a40038ba2d
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-16 18:09:19 +0200

    add day, week, month, schedule calendar to main

--- lib/views/schedule/schedule_view.dart
+++ lib/views/schedule/schedule_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class ScheduleView extends StatefulWidget {
   const ScheduleView({super.key});
@@ -8,6 +9,9 @@ class ScheduleView extends StatefulWidget {
 }
 
 class _ScheduleViewState extends State<ScheduleView> {
+  Key _calendarKey = UniqueKey();
+  CalendarView _calendarView = CalendarView.month;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +19,74 @@ class _ScheduleViewState extends State<ScheduleView> {
         title: const Text('My Schedule'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Schedule'),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _calendarView = CalendarView.day;
+                    _calendarKey = UniqueKey();
+                  });
+                },
+                child: const Text('Day'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _calendarView = CalendarView.week;
+                    _calendarKey = UniqueKey();
+                  });
+                },
+                child: const Text('Week'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _calendarView = CalendarView.month;
+                    _calendarKey = UniqueKey();
+                  });
+                },
+                child: const Text('Month'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _calendarView = CalendarView.schedule;
+                    _calendarKey = UniqueKey();
+                  });
+                },
+                child: const Text('Schedule'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: SfCalendar(
+              key: _calendarKey,
+              view: _calendarView,
+              firstDayOfWeek: 1,
+              onViewChanged: (ViewChangedDetails details) {},
+              selectionDecoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                  width: 2,
+                ),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(8),
+                ),
+                shape: BoxShape.rectangle,
+              ),
+              showNavigationArrow: true,
+              showWeekNumber: true,
+              weekNumberStyle: WeekNumberStyle(
+                backgroundColor: Colors.teal.shade100,
+              ),
+              monthViewSettings: const MonthViewSettings(showAgenda: true),
+            ),
+          ),
+        ],
       ),
     );
   }
