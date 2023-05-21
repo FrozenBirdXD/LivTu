36388e516f10546f06dd04dc8b13f877a895644a
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-21 23:07:32 +0200

    add profile picture and icon to page (tmp picture)

--- lib/views/profile/profile_view.dart
+++ lib/views/profile/profile_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:livtu/constants/routes.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:livtu/services/auth/auth_service.dart';
 
 class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
@@ -10,6 +11,9 @@ class ProfileView extends StatefulWidget {
 }
 
 class _ProfileViewState extends State<ProfileView> {
+  final double coverHeight = 280;
+  final double profileHeight = 160;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,37 +21,133 @@ class _ProfileViewState extends State<ProfileView> {
         title: Text(AppLocalizations.of(context)!.profile),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              AppLocalizations.of(context)!.profileOptions,
-              style: const TextStyle(
-                fontSize: 24,
-              ),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          buildTop(),
+          buildProfile(),
+          buildOptionButtons(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 48,
+      ),
+      child: Column(
+        children: [
+          Text(
+            AuthService.firebase().currentUser?.displayName ??
+                'User${AuthService.firebase().currentUser!.id}',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 28.0,
             ),
-            const SizedBox(
-              height: 32,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(changeUsernameRoute);
-              },
-              child: Text(AppLocalizations.of(context)!.changeUsername),
-            ),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(changePasswordRoute);
-              },
-              child: Text(AppLocalizations.of(context)!.changePassword),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-          ],
+          ),
+          Text(
+            AuthService.firebase().currentUser?.email ??
+                AppLocalizations.of(context)!.notRegistered,
+          ),
+          const Divider(
+            height: 1,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Stack buildTop() {
+    final top = coverHeight - profileHeight / 2;
+    final bottom = profileHeight / 2;
+
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            bottom: bottom,
+          ),
+          child: buildCoverImage(),
         ),
+        Positioned(
+          top: top,
+          child: buildProfileImage(),
+        ),
+      ],
+    );
+  }
+
+  CircleAvatar buildProfileImage() {
+    return CircleAvatar(
+      radius: profileHeight / 2,
+      backgroundColor: Colors.grey.shade800,
+      backgroundImage: const NetworkImage(
+        'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+      ),
+      child: Container(
+        width: profileHeight,
+        height: profileHeight,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.grey.shade100,
+            width: 5.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildCoverImage() {
+    return Container(
+      color: Colors.grey,
+      child: Image.network(
+        'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        width: double.infinity,
+        height: coverHeight,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Center buildOptionButtons() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            AppLocalizations.of(context)!.profileOptions,
+            style: const TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(changeUsernameRoute);
+            },
+            child: Text(AppLocalizations.of(context)!.changeUsername),
+          ),
+          const SizedBox(height: 8),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(changePasswordRoute);
+            },
+            child: Text(AppLocalizations.of(context)!.changePassword),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
       ),
     );
   }
