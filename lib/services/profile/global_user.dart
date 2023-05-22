8faa40f47cf5f978d40f8a4b158e40d2b0b91a81
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-22 22:30:24 +0200

    update global user

--- lib/services/profile/global_user.dart
+++ lib/services/profile/global_user.dart
@@ -1,13 +1,28 @@
-import 'package:livtu/services/auth/auth_user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:livtu/services/profile/global_user_constants.dart';
 
 class GlobalUser {
-  final AuthUser user;
+  final String documentId;
+  final String userId;
+  final String? photoURL;
+  final String? displayName;
   final String? iconURL;
   final String? description;
 
   GlobalUser({
-    required this.user,
+    required this.photoURL,
+    required this.displayName,
     required this.iconURL,
-    required this.description,
+    required this.description, 
+    required this.documentId,
+    required this.userId,
   });
+
+  GlobalUser.fromSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+      : documentId = snapshot.id,
+        userId = snapshot.data()[userIdFieldName],
+        photoURL = snapshot.data()[photoURLFieldName],
+        displayName = snapshot.data()[displayNameFieldName],
+        iconURL = snapshot.data()[iconURLFieldName],
+        description = snapshot.data()[descriptionFieldName] as String;
 }
