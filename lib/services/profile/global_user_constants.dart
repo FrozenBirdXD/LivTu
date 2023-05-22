3dc3a380b5d49706ca3b834674bbb7c4dfe11234
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-22 22:30:39 +0200

    global user utils

---
+++ lib/services/profile/global_user_constants.dart
@@ -0,0 +1,5 @@
+const photoURLFieldName = 'photoURL';
+const iconURLFieldName = 'iconURL';
+const displayNameFieldName = 'displayName';
+const descriptionFieldName = 'description';
+const userIdFieldName = 'userId';
---
+++ lib/services/profile/global_user_exceptions.dart
@@ -0,0 +1,13 @@
+class GlobalUserException implements Exception {
+  const GlobalUserException();
+}
+
+class CouldNotUpdatePhotoURL implements GlobalUserException {}
+
+class CouldNotUpdateDisplayName implements GlobalUserException {}
+
+class CloudNotUpdateIconURL implements GlobalUserException {}
+
+class CouldNotUpdateDescription implements GlobalUserException {}
+
+class GlobalUserNotFound implements GlobalUserException {}
