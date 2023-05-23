d2e5bcf0e34681a418d4e73916bdf177232980a6
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-23 19:39:21 +0200

    add method to get urls

--- lib/services/profile/global_user_helper.dart
+++ lib/services/profile/global_user_helper.dart
@@ -1,19 +1,30 @@
-import 'package:flutter/material.dart';
 import 'package:livtu/services/profile/global_user_exceptions.dart';
 import 'package:livtu/services/profile/global_user_service.dart';
-import 'package:livtu/utils/dialogs/error_dialog.dart';
 
-Future<String> getUserName(BuildContext context) async {
+Future<String> getUserName() async {
   GlobalUserService userService = GlobalUserService();
-    try {
-      String displayName = await userService.getDisplayName();
-      if (displayName.isEmpty) {
-        return 'username not set';
-      } else {
-        return displayName;
-      }
-    } on GlobalUserException {
-      await showErrorDialog(context, 'Username not found');
-      throw CouldNotGetDisplayName();
+  try {
+    String displayName = await userService.getDisplayName();
+    if (displayName.isEmpty) {
+      return 'username not set';
+    } else {
+      return displayName;
     }
+  } on GlobalUserException {
+    throw CouldNotGetDisplayName();
   }
+}
+
+Future<String> getIconURL() async {
+  GlobalUserService userService = GlobalUserService();
+  try {
+    String iconURL = await userService.getIconURL();
+    if (iconURL.isEmpty) {
+      return 'https://firebasestorage.googleapis.com/v0/b/livtu-flutter.appspot.com/o/profile_images%2Fdefault%20icon.png?alt=media&token=c33ce1c3-f961-4c3e-ae7d-41da985659d9';
+    } else {
+      return iconURL;
+    }
+  } on GlobalUserException {
+    throw CouldNotGetIconURL();
+  }
+}
